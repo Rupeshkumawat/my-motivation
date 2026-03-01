4cf91630b429665272e2de7962187c476bcc76af
Rupeshkumawat <kumawatrupesh049@gmail.com>
2026-03-01 17:34:40 +0530

    Add motivation.py for user interaction with AI

---
+++ motivation.py
@@ -0,0 +1,71 @@
+from groq import Groq
+import openai
+import requests
+import datetime
+#import os
+import streamlit as st
+#from dotenv import load_dotenv
+#load_dotenv()
+#import os
+openai.api_key = st.secrets["OPENAI_API_KEY"]  #api_key=st.secrets["OPENAI_API_KEY"]
+#print("the api key is ", os.getenv("OPENAI_API_KEY"))
+#client = OpenAI(api_key="KngEsfDmjmHorBGnxKLr2mieuELUjRoFdli7ajyBftpFj5PibIkfvcVLukEbtgDt4-2gxVnumT3BlbkFJp4Ospqv2VR1gat8LaUSsXz0j4QaCy-vk_96_br2PvuArMbK4IMcCfEh23li1yftbKvgA6sLsUA")
+#client = openai(api_key)
+client = Groq(
+        api_key=st.secrets["GROQ_API_KEY"],
+)
+# Set the app title
+st.title("🤖🤖 Your Motivation Expert")
+st.title("Welcome to the AI Assistant 🎉")
+st.write("Select your choice:")
+
+choice = st.radio(
+    "Pick your vibe:",
+    [
+        "Motivational Speaker (Akshay Kumar)",
+        "Strict Teacher (Gabbar Singh)",
+        "Yoga Instructor (Baba Ramdev)"
+    ]
+)
+
+st.write(f"You selected: **{choice}** 🚀")
+#choice = int(input("Enter your choice: "))
+
+if choice == "Motivational Speaker (Akshay Kumar)":
+    SYSTEM_PROMPT = """You are a helpful Motivational speaker AI assistant who is specialized in boosting user's confidence by resolving the user query in positive manner.
+    You give response in bollywood actor Akshay Kumar's style. A light weight comedy style is must. Always be witty, punchy, comedy and in light mood. 
+    Make the other person forget their issue"""
+elif choice == "Strict Teacher (Gabbar Singh)":
+    SYSTEM_PROMPT = """You are a strict teacher AI assistant who is specialized in resolving user query in a very strict and disciplined manner.
+    You give response in Bollywood actor Gabbar Singh's style. Gabbas singh is famouse character in sholay movie. 
+    Add "Are o Samba" style in your reply. You don't tolerate any nonsense. Give reply in max 5 - 6 punchy lines to motivate and forget his/her pains. """
+elif choice == "Yoga Instructor (Baba Ramdev)":
+    SYSTEM_PROMPT = """You are a Yoga instructor AI assistant who is specialized in resolving user query in a very calm and peaceful manner.
+    You give response in Indian yoga guru Baba Ramdev's style. You always promote yoga and healthy lifestyle.
+    Always reply in Hindi language. Give some ayurvedic tips, pranayam tips and spiritual advice to solve problems and motivate.
+    Add "Yoga se hoga" at the end of each response."""
+
+messages = [
+    {"role":"system", "content": SYSTEM_PROMPT}
+]
+#query=""
+#while True:
+    # User input
+    # query = st.chat_input("How can I help you today...")
+query = st.text_input("Type your message:", value="")
+#query = input(">Enter your query: ")
+if query and query.lower() in ["exit", "quit", "bye","end"]:
+    st.markdown("Goodbye!")
+    #break
+messages.append({"role":"user", "content": query})
+#response = openai.chat.completions.create(
+#     model="gpt-3.5-turbo",
+#     messages=messages
+#)
+response = client.chat.completions.create(
+        messages=messages,
+        model="llama-3.3-70b-versatile"
+     )
+    #print(response.choices[0].message.content)
+st.markdown(response.choices[0].message.content)
+#print(response.output_text)
